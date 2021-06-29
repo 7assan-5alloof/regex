a6627b76c3d9be637110ed8692dd91ef7d280025
7assan-5alloof <hassankhalloof20@gmail.com>
2021-06-29 19:37:45 +0400

    Code (Incomplete)

---
+++ main.py
@@ -0,0 +1,12 @@
+import re as regex  # To be used for spotting numbers throughout the text
+
+# Find numbers in each line of the file, and add all the results up
+file = open(input("Enter file name: "))
+sum = int()
+for line in file:
+    pass
+#     for number in numbers:
+#         sum += int(number)
+#
+# # Sum
+# print("Sum of inline numbers:", sum)
