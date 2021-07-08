5cc9459bb18227bb60882d05542cffe407af6576
7assan-5alloof <hassankhalloof20@gmail.com>
2021-07-08 10:25:07 +0400

    Solve Problem: New Lines Disallowed

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import re as regex  # To be used for spotting numbers throughout the text
+import re  # To be used for spotting numbers throughout the text
 
 # Find numbers in each line of the file, and add all the results up
 file = open(input("Enter file name: "))
 sum = int()
+count = int()
 for line in file:
-    pass
-#     for number in numbers:
-#         sum += int(number)
-#
-# # Sum
-# print("Sum of inline numbers:", sum)
+    line = line.rstrip()  # Important for finding the numbers
+    numbers = re.findall("[0-9]+", line)
+    if len(numbers) > 0:  # if numbers were found in the line
+        for number in numbers:
+            count += 1
+            sum += int(number)  # Numbers are going to be 'str' type when added
+            # to numbers by regex.findall()
+
+print("Count:", count)
+print("Sum:", sum)
